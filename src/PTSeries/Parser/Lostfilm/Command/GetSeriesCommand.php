7399e9f6ac262fe8770a02e2eb45e76b67567d53
Илья Гусев <mail@igusev.ru>
2015-04-04 20:00:39 +0300

    getSeriesCommand

--- src/PTSeries/Parser/Lostfilm/Command/GetSeriesCommand.php
+++ src/PTSeries/Parser/Lostfilm/Command/GetSeriesCommand.php
@@ -3,6 +3,7 @@
 namespace PTSeries\Parser\Lostfilm\Command;
 
 use CLIFramework\Command;
+use PTSeries\Parser\Lostfilm\LostfilmApi;
 
 class GetSeriesCommand extends Command
 {
@@ -13,7 +14,12 @@ class GetSeriesCommand extends Command
         $logger->info('execute');
         $logger->error('error');
 
-        $input = $this->ask('Please type something');
+//        $input = $this->ask('Please type something');
 
+
+        $lostfilm = new LostfilmApi();
+
+
+        var_dump($lostfilm->getSeries());
     }
 }
