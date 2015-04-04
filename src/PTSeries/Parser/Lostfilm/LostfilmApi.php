82c2df1de1cba54e2c8442af622807c5b8a29a4b
Илья Гусев <mail@igusev.ru>
2015-04-04 19:59:29 +0300

    lostfilm api class init

---
+++ src/PTSeries/Parser/Lostfilm/LostfilmApi.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PTSeries\Parser\Lostfilm;
+
+use SleepingOwl\Apist\Apist;
+
+class LostfilmApi extends Apist
+{
+    public function getBaseUrl()
+    {
+        return 'http://www.lostfilm.tv/';
+    }
+
+    public function getSeries()
+    {
+       
+    }
+}
