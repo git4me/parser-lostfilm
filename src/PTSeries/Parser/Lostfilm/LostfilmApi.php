c3a959048307c29f50e2f91e1447ad379b443de7
Илья Гусев <mail@igusev.ru>
2015-04-04 20:00:19 +0300

    getSeries method

--- src/PTSeries/Parser/Lostfilm/LostfilmApi.php
+++ src/PTSeries/Parser/Lostfilm/LostfilmApi.php
@@ -13,6 +13,16 @@ class LostfilmApi extends Apist
 
     public function getSeries()
     {
-       
+        return $this->get('/serials.php', [
+            'series' => Apist::filter('#MainDiv .mid .bb .bb_a')->each([
+                'name' => Apist::current()->html()->call(function ($name) {
+                    return substr($name, 0, strpos($name, '<br>'));
+                }),
+                'original_name' => Apist::current()->filter('span')->trim('()'),
+                'local_id' => Apist::current()->attr('href')->call(function ($href) {
+                    return (int) str_replace('/browse.php?cat=', '', $href);
+                })
+            ])
+        ]);
     }
 }
