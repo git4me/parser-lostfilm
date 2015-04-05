9406411af77ccd8d60eb8dfada50c74c0dcdbd8d
Илья Гусев <mail@igusev.ru>
2015-04-06 02:27:46 +0300

    parsing series pages

--- src/PTSeries/Parser/Lostfilm/LostfilmApi.php
+++ src/PTSeries/Parser/Lostfilm/LostfilmApi.php
@@ -31,7 +31,27 @@ class LostfilmApi extends Apist
         return $this->get('/browse.php',
             [
                 'name' => Apist::filter('#MainDiv .mid h1:first')->text(),
-                'description' => Apist::filter('#MainDiv .mid div.content + span:first')->text()
+                'description' => Apist::filter('#MainDiv .mid div.content + span:first')->text(),
+                'episodes' => Apist::filter('#MainDiv .mid div.t_row')->each([
+                    'season' => Apist::current()
+                        ->filter('table td nobr div a.a_discuss')
+                        ->attr('href')->call(function ($href) {
+                            parse_str(parse_url($href, PHP_URL_QUERY), $params);
+                            return (int) $params['s'];
+                        }),
+                    'episode' => Apist::current()
+                        ->filter('table td nobr div a.a_discuss')
+                        ->attr('href')->call(function ($href) {
+                            parse_str(parse_url($href, PHP_URL_QUERY), $params);
+                            return (int) $params['e'];
+                        }),
+                    'name' => Apist::current()->filter('td.t_episode_title div div nobr span')->text()->trim(),
+                    'original_name' => Apist::current()
+                        ->filter('td.t_episode_title div div nobr')
+                        ->html()->call(function ($name) {
+                            return substr($name, strpos($name, '<br>') + 4);
+                        })->trim('()')
+                ])
             ],
             [
                 'query' => [
